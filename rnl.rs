bb8994851f414d8bece0836c4fe45c189395c696
Christopher Ray <vde435@gmail.com>
2023-07-15 12:48:11 -0500

    Update rnl.rs

--- rnl.rs
+++ rnl.rs
@@ -1,19 +1,20 @@
 //Remove New Lines
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let input_filename = &args[1];
+    let output_filename = &args[2];
     
-    let mut file = File::open(filename)?;
+    let mut file = File::open(input_filename)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
     let new_contents = contents.replace("\n", "");
 
-    println!("{}", new_contents);
+    fs::write(output_filename, new_contents)?;
 
     Ok(())
 }
